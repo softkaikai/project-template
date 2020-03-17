2723fb44fda9787e7fa12baa47bebf05d81de3f4
wenkai <282748212@qq.com>
2020-03-17 11:10:01 +0800

    优化文件复数命名

---
+++ vue/src/views/login/resetPassword/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        this is LoginResetPasswordIndex
+    </div>
+</template>
+<script>
+
+    export default {
+        name: 'LoginResetPasswordIndex',
+        data () {
+            return {
+
+            };
+        },
+        computed: {
+
+        },
+        watch: {
+
+        },
+        mounted () {
+
+        },
+        methods: {
+
+        }
+    };
+</script>
+<style lang="less" scoped>
+
+</style>
